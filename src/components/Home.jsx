1ed671a3ca23a31a213f467b701412f389e8a2de
WendolynQuartey <quarteywendolyn@gmail.com>
2026-02-25 13:17:30 -0500

    Basic layout setup

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import Nav from "./Nav.jsx";
+import Search from "./Search.jsx";
+
+export default function Home() {
+   return (
+      <>
+         <Nav />
+         <Search />
+      </>
+   )
+}
