f210a17828fc015faf1005612e2fa54520d58c05
WendolynQuartey <quarteywendolyn@gmail.com>
2026-03-02 12:29:00 -0500

    fixed search bar functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,26 @@
-export default function Search() {
+import {useState} from "react";
+import axios from "axios";
+
+export default function Search({onSearch}) {
+   const [searchTerm, setSearchTerm] = useState("");
+
+   async function handleSearch(e){
+      e.preventDefault;
+      if(!searchTerm.trim()) return;
+
+      try {
+         const res = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`);
+         onSearch(res.data);
+      } catch (error) {
+         console.error(error.message);
+      }
+   }
+
    return (
-      <>
+      <form onChange={handleSearch}>
       <label>Search
-         <input type="text" name="searchBar" id="" />
+         <input type="text" name="searchBar" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search for a meal..."/>
       </label>
-      </>
+      </form>
    )
 }
